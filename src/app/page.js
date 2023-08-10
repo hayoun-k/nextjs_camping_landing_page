5c83b01cd6e7400924284137e22d1a938d9ad469
hayounk <hayoun.k.1102@gmail.com>
2023-08-10 13:43:17 -0700

    alt added

--- src/app/page.js
+++ src/app/page.js
@@ -11,12 +11,12 @@ export default function Home() {
   return (
     <div className="wrapper">
       <div className="image-container">
-        <img src="/img/bird-1.svg" className='bird1' id='bird1' />
-        <img src="/img/bird-2.svg" className='bird2' id='bird2'/>
-        <img src="/img/img-1.svg" className='tent' id='tent'/>
-        <img src="/img/img-2.svg" className='ground' id='ground'/>
-        <img src="/img/img-3.svg" className='tree' id='tree'/>
-        <img src="/img/img-4.svg" className='sky' id='sky'/>
+        <img src="/img/bird-1.svg" className='bird1' id='bird1' alt='bird image' />
+        <img src="/img/bird-2.svg" className='bird2' id='bird2' alt='bird image'/>
+        <img src="/img/img-1.svg" className='tent' id='tent' alt='tent image'/>
+        <img src="/img/img-2.svg" className='ground' id='ground' alt='ground image'/>
+        <img src="/img/img-3.svg" className='tree' id='tree' alt='tree image'/>
+        <img src="/img/img-4.svg" className='sky' id='sky' alt='sky image'/>
       </div>
       <Header />
       <Main />
