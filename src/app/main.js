2776da0d77c68a9cfbda0d9be142e8b69441533a
hayounk <hayoun.k.1102@gmail.com>
2023-08-10 13:44:24 -0700

    href added

--- src/app/main.js
+++ src/app/main.js
@@ -7,7 +7,7 @@ export default function Main() {
         <h2 className="subtitle uppercase">a lifetime experience</h2>
         <h1>Camping Events<br />To Remember</h1>
         <p className="description">Camp anywhere anytime without hassle and enjoy</p>
-        <a className="btn btn-primary">Explore Locations</a>
+        <a href='#' className="btn btn-primary">Explore Locations</a>
       </div>
     </main>
   )
