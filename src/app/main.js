bd7e51cd5077cbbd123f9ee37efade37528deecc
hayounk <hayoun.k.1102@gmail.com>
2023-08-10 13:20:53 -0700

    camping landing page nextjs

--- src/app/main.js
+++ src/app/main.js
@@ -1,10 +1,14 @@
+import Image from "next/image"
+
 export default function Main() {
   return (
     <main>
-      <h2 className="subtitle uppercase">a lifetime experience</h2>
-      <h1>Camping Events<br />To Remember</h1>
-      <p className="description">Camp anywhere anytime without hassle and enjoy</p>
-      <a className="btn btn-primary">Explore Locations</a>
+      <div class="text-container">
+        <h2 className="subtitle uppercase">a lifetime experience</h2>
+        <h1>Camping Events<br />To Remember</h1>
+        <p className="description">Camp anywhere anytime without hassle and enjoy</p>
+        <a className="btn btn-primary">Explore Locations</a>
+      </div>
     </main>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,23 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
 import Main from './main'
 import Header from './header'
 import Footer from './footer'
+import { useEffect } from 'react'
 
 export default function Home() {
+
   return (
     <div className="wrapper">
+      <div className="image-container">
+        <img src="/img/bird-1.svg" className='bird1' id='bird1' />
+        <img src="/img/bird-2.svg" className='bird2' id='bird2'/>
+        <img src="/img/img-1.svg" className='tent' id='tent'/>
+        <img src="/img/img-2.svg" className='ground' id='ground'/>
+        <img src="/img/img-3.svg" className='tree' id='tree'/>
+        <img src="/img/img-4.svg" className='sky' id='sky'/>
+      </div>
       <Header />
       <Main />
       <Footer />
